a28f4e38598c09e4fdcaebdb28ae605b4404166e
yongning0310 <85162884+yongning0310@users.noreply.github.com>
2023-10-03 21:36:23 +0800

    merge with latest push

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -10,10 +10,6 @@ import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.internship.Internship;
-<<<<<<< HEAD
-=======
-import seedu.address.model.person.Person;
->>>>>>> f0002730 (refactored storage and parsers)
 
 /**
  * Deletes an internship identified using it's displayed index from the address book.
