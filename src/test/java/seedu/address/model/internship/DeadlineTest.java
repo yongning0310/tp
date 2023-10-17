d6d9b41b0ab236bd2da1634d33fcaf7b1de2c5cd
jinyang628 <81862152+jinyang628@users.noreply.github.com>
2023-10-17 15:30:27 +0800

    Fix styling errors

--- src/test/java/seedu/address/model/internship/DeadlineTest.java
+++ src/test/java/seedu/address/model/internship/DeadlineTest.java
@@ -1,7 +1,5 @@
 package seedu.address.model.internship;
 
-import seedu.address.model.requirement.Requirement;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
