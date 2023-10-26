61c64df7d61eac2b75175330986f3ab0347feb30
yongning0310 <85162884+yongning0310@users.noreply.github.com>
2023-10-26 22:39:00 +0800

    added test case for index

--- src/test/java/seedu/address/commons/core/index/IndexTest.java
+++ src/test/java/seedu/address/commons/core/index/IndexTest.java
@@ -1,10 +1,10 @@
 package seedu.address.commons.core.index;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 public class IndexTest {
 
