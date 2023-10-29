6facc16bed29de28fe5e8ef6a8e3c249cc786d56
jinyang628 <81862152+jinyang628@users.noreply.github.com>
2023-10-29 16:44:02 +0800

    Rectify style errors

--- src/test/java/seedu/address/model/internship/RequirementContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/internship/RequirementContainsKeywordsPredicateTest.java
@@ -1,8 +1,6 @@
 package seedu.address.model.internship;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
